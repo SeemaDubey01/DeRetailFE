193c64e3f991edd273a77972b6d18af0fcbab9e8
SeemaDubey01 <seemachoube2@gmail.com>
2021-01-29 23:08:36 +0100

    new bill item edit

--- js/NewBilling.js
+++ js/NewBilling.js
@@ -1,9 +1,8 @@
-var index = 0;
+var glIndex = 0;
 var wsSellDetailsArray = [];
 $(document).ready(function(){
     /* write current date and time on page */ 
     let d = new Date();
-    alert ('timestamp : ' + d.getFullYear() + '-' + ('0' + (d.getMonth() + 1)).slice(-2) + '-' + ('0' + d.getDate()).slice(-2) + ' ' + d.toTimeString().substr(0,8));
     $('#date-today').html('<b>Date :</b> ' + d.toDateString() + '   <b>Time:</b> ' + d.toTimeString().substr(0,8));
     
     /* change background color of input boxes */
@@ -25,7 +24,7 @@ function inputBgColor(){
     $('input').focus(function(){$(this).select();})
 }
  /* Calculate total when unit price or quantity changes */
-function calculateTotalPrice(index){
+function calculateTotalPrice(){
     let totalPrice = 0;
     $('#quantity').focusout(function(){
         totalPrice = $('#quantity').val() * $('#unitPrice').val();
@@ -38,20 +37,64 @@ function calculateTotalPrice(index){
 }
 function resetNewBill(){
     $('#billing-table').html('<tbody><tr class="table-light"><th>#</th><th>Description</th><th>Unit Price</th><th>Qty</th><th>Total Price</th></tr></tbody>');
-    index = 0;
+    glIndex = 0;
     wsSellDetailsArray = [];
-    calculateBillAmount();''
+    calculateBillAmount();
 }
 function addItem(){
     /*if($('#productCode').val()==0){
         alert ("product code can not be blank");
         return;
     } */
-    index++;
-    $('#billing-table').append('<tr><td>'+index+'</td><td>'+$('#productDescr').val()+'</td><td>'+$('#unitPrice').val()+'</td><td>'+$('#quantity').val()+'</td><td>'+$('#totalPrice').val()+'</td></tr>');
+    glIndex++;
+    tempHtml =  '<tr><td>'+glIndex+'</td><td>'+$('#productDescr').val()+'</td>';
+    tempHtml += '<td id="tdUnitPrice-'+glIndex+'">'+$('#unitPrice').val()+'</td>';
+    tempHtml += '<td id="tdQuantity-'+glIndex+'">'+$('#quantity').val()+'</td>'
+    tempHtml += '<td id="tdTotalPrice-'+glIndex+'">'+$('#totalPrice').val()+'</td>';
+    tempHtml += '<td onclick="tdEditQuantity('+glIndex+')">';
+    //tempHtml += '<td onclick="tdEditQuantity(\'tdUnitPrice-'+glIndex+'\',\'tdQuantity-'+glIndex+'\',\'tdTotalPrice-'+glIndex+'\', '+glIndex+')">';
+    tempHtml += '<span class="btn fas fa-edit text-primary m-width-td-1" data-toggle="tooltip" title="Update"></span></td></tr>';
+    $('#billing-table').append(tempHtml);
     storeWSSellDetails();
     initAddForm();
     calculateBillAmount();
+}
+function tdEditQuantity(wIndex){
+  //  alert (" tdEditQuantity : " + $('#tdUnitPrice-'+wIndex).text() + ", " + $('#tdQuantity-'+wIndex).text() + ", " + $('#tdTotalPrice-'+wIndex).text());
+    tempQuantity = $('#tdQuantity-'+wIndex).text();
+    $('#tdQuantity-'+wIndex).html('<input type="number" class="form-control m-width-td-4" value="'+ tempQuantity +'" id="in-tdQuantity-'+wIndex+'" onfocusout="tdEditQuantityDone('+wIndex+')">');
+    $('#in-tdQuantity-'+wIndex).select();
+}
+function tdEditQuantityDone(wIndex){
+    let totalBill = 0.00;
+    tempQuantity = $('#in-tdQuantity-'+wIndex).val();
+    totalBill = +$('#tdUnitPrice-'+wIndex).text() * tempQuantity;
+    
+    $('#tdQuantity-'+wIndex).text(tempQuantity);
+    $('#tdTotalPrice-'+wIndex).text(totalBill.toFixed(2));
+
+    wsSellDetailsArray[wIndex-1].quantity = tempQuantity;
+    wsSellDetailsArray[wIndex-1].totalPrice = totalBill;
+ 
+    calculateBillAmount()
+}
+function populateSellDetails(){
+	let totalBill = 0.00;
+	wsSellDetailsArray.forEach(function(item,index){
+		glIndex++;
+		$('#billing-table').append('<tr><td>'+glIndex+'</td><td>'+item.productDescr+'</td><td>'+item.unitPrice+'</td><td>'+item.quantity+'</td><td>'+item.totalPrice+'</td></tr>');
+		totalBill = +totalBill + +item.totalPrice;
+	});
+	gst = +totalBill * 0 / 100;
+    payable = +totalBill + gst;
+
+    $('#totalBill').text(totalBill.toFixed(2));
+    $('#gst').text(gst.toFixed(2));
+    $('#payable').text(payable.toFixed(2));
+
+    $('#pay-by-card-amt').text(payable.toFixed(2));
+    $('#pay-by-cash-amt').text(payable.toFixed(2));
+    $('#reset-details').prop('disabled',true);
 }
 function initAddForm(){
     let amt =0;
@@ -60,6 +103,8 @@ function initAddForm(){
     $('#unitPrice').val(amt.toFixed(2));
     $('#quantity').val(0);
     $('#totalPrice').val(amt.toFixed(2));
+    $('#pay-by-card-amt').text(amt.toFixed(2));
+    $('#pay-by-cash-amt').text(amt.toFixed(2));
     $('#productCode').select();
 }
 function calculateBillAmount(){
@@ -67,10 +112,9 @@ function calculateBillAmount(){
     let gst = 0.00;
     let payable = 0.00;
     wsSellDetailsArray.forEach(function (item, index){
-    //    console.log("index: " + index + " amt: " + item.totalPrice);
         totalBill = +totalBill + +item.totalPrice;
     });
-    gst = totalBill * 18 / 100;
+    gst = +totalBill * 0 / 100;
     payable = +totalBill + gst;
 
     $('#totalBill').text(totalBill.toFixed(2));
@@ -81,8 +125,11 @@ function calculateBillAmount(){
     $('#pay-by-cash-amt').text(payable.toFixed(2));
 }
 function billSubmit(){
+    let d = new Date();
+    let dateSold =  d.getFullYear() + '-' + ('0' + (d.getMonth() + 1)).slice(-2) + '-' + ('0' + d.getDate()).slice(-2) + ' ' + d.toTimeString().substr(0,8);
+    $('#dateSold').val(dateSold);
     $('#user').val('admin');
-    $('#sellDetails').val(wsSellDetailsArray);
+  //  $('#sellDetails').val(wsSellDetailsArray);
     wsSellDetailsArray.forEach(function(item,index){
     	$('#billing-form').append('<input type="hidden" name="sellDetails['+index+'].itemNo" value="'+item.itemNo+'">');
     	$('#billing-form').append('<input type="hidden" name="sellDetails['+index+'].productCode" value="'+item.productCode+'">');
@@ -93,7 +140,7 @@ function billSubmit(){
     $('#billing-form').submit();
 }
 function storeWSSellDetails(){
-    var wsSellDetails = new WSSellDetails(index-1, $('#productCode').val(),$('#productDescr').val(),$('#unitPrice').val(),$('#quantity').val(),$('#totalPrice').val());
+    var wsSellDetails = new WSSellDetails(glIndex, $('#productCode').val(),$('#productDescr').val(),$('#unitPrice').val(),$('#quantity').val(),$('#totalPrice').val());
     wsSellDetailsArray.push(wsSellDetails);
 }
 function WSSellDetails(wsIndex, wsProductCode, wsProductDescr, wsUnitPrice, wsQuantity){
